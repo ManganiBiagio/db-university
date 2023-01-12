05d1c2f82f8bb57fff3715dcf540cb38c709048a
ManganiBiagio <biagiomangani@live.it>
2023-01-12 15:14:04 +0100

    MILESTONE 2

--- join.sql
+++ join.sql
@@ -22,7 +22,32 @@ INNER JOIN `courses`
 WHERE `teachers`.`id`=44;
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e 
 --ilrelativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT *
+FROM `students`
+INNER JOIN `degrees`
+        ON `students`.`degree_id`=`degrees`.`id`
+INNER JOIN `departments`
+        ON `departments`.`id`=`degrees`.`department_id`
+ORDER BY `students`.`surname` ASC,
+         `students`.`name` ASC
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT * 
+FROM `degrees`
+INNER JOIN `courses`
+        ON `degrees`.`id`=`courses`.`degree_id`
+INNER JOIN `course_teacher`
+        ON `course_teacher`.`course_id`=`courses`.`id`
+INNER JOIN `teachers`
+        ON `teachers`.`id`=`course_teacher`.`teacher_id`;
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT * FROM `teachers`
+INNER JOIN `course_teacher`
+       ON `teachers`.`id`=`course_teacher`.`teacher_id`
+INNER JOIN `courses`
+       ON `course_teacher`.`course_id`=`courses`.`id`
+INNER JOIN `degrees`
+       ON `degrees`.`id`=`courses`.`degree_id`
+INNER JOIN `departments`
+       ON `departments`.`id`=`degrees`.`department_id`
+WHERE `departments`.`name`="Dipartimento di Matematica";
 --7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto persuperare ciascuno dei suoi esami
